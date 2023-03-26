f96a1ca60a4e4b2b755fbeb9ec0a8f4f1de50f3c
VadimPaharenko <Voth3@yandex.ru>
2023-03-26 14:53:37 +0700

    Homework 7 ver2

--- 07/keylist.erl
+++ 07/keylist.erl
@@ -8,26 +8,26 @@ loop(#state{list = List, counter = Counter} = State) when is_list(List), is_inte
     receive
         {From, add, Key, Value, Comment} -> 
             NewState = State#state{list = [{Key, Value, Comment} | List], counter = Counter + 1},
-            From ! {ok, NewState#state.counter},
+            From ! {ok, NewState},
             loop(NewState);
         {From, is_member, Key} ->
-            lists:keymember(Key, 1, List),
+            Result = lists:keymember(Key, 1, List),
             NewState = State#state{counter = Counter + 1},
-            From ! {ok, NewState#state.counter},
+            From ! {Result, NewState},
             loop(NewState);
         {From, take, Key} ->
-            {_, _, TupleList} = lists:keytake(Key, 1, List),
+            {_, Tuple, TupleList} = lists:keytake(Key, 1, List),
             NewState = State#state{list = TupleList, counter = Counter + 1},
-            From ! {ok, NewState#state.counter},
+            From ! {Tuple, NewState},
             loop(NewState);
         {From, find, Key} ->
-            lists:keyfind(Key, 1, List),
+            Tuple = lists:keyfind(Key, 1, List),
             NewState = State#state{counter = Counter + 1},
-            From ! {ok, NewState#state.counter},
+            From ! {Tuple, NewState},
             loop(NewState);
         {From, delete, Key} ->
             NewState = State#state{list = lists:keydelete(Key, 1, List), counter = Counter + 1},
-            From ! {ok, NewState#state.counter},
+            From ! {ok, NewState},
             loop(NewState)
         end.
 
