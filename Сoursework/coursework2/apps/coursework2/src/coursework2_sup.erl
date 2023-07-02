c4479109563b41787e0668fbd1d3b5caa44dbc8c
VadimPaharenko <Voth3@yandex.ru>
2023-07-02 20:34:14 +0700

    coursework

--- Сoursework/coursework2/apps/coursework2/src/coursework2_sup.erl
+++ Сoursework/coursework2/apps/coursework2/src/coursework2_sup.erl
@@ -16,20 +16,27 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%% sup_flags() = #{strategy => strategy(),         % optional
-%%                 intensity => non_neg_integer(), % optional
-%%                 period => pos_integer()}        % optional
-%% child_spec() = #{id => child_id(),       % mandatory
-%%                  start => mfargs(),      % mandatory
-%%                  restart => restart(),   % optional
-%%                  shutdown => shutdown(), % optional
-%%                  type => worker(),       % optional
-%%                  modules => modules()}   % optional
 init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
     ChildSpecs = [],
+    
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/abonent", abonent_http_handler, []},
+            {"/abonents", abonents_http_handler, []},
+            {"/call", call_http_handler, []},
+            {"/", coursework_http_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{
+        env => #{dispatch => Dispatch}}
+    ),
+
+    dialer:start(),
+    database:start(),
+
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
